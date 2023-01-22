bd6cefffe744b62d9a45d233a0070a977799be18
Marmen82 <mendez.marcela1982@gmail.com>
2023-01-22 20:23:08 -0300

    Se agrega el proyecto

---
+++ codigo.js
@@ -0,0 +1,8 @@
+
+alert ( " ¡Hola!, gracias por visitar mi Curriculum Vitae. ");
+
+
+document.getElementById('check_item').addEventListener('click', function darkMode() {
+    let main_body = document.body;
+    main_body.classList.toggle("dark_mode");
+})
